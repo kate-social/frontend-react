35e0a437a28e0401e26c99f66ec0ab63222ec70e
Akionka <42269025+Akionka@users.noreply.github.com>
2021-03-14 12:32:53 +0300

    :sparkles: Set up some redux stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-const App = () => {
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { initializeApp } from './redux/app-reducer'
+import { useCookies } from 'react-cookie'
+
+export const App = () => {
+  const [cookies] = useCookies(['token'])
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeApp(cookies.token))
+  }, [dispatch, cookies.token])
+
   return (
-    <div className="App">
+    <div className='App'>
+      Hello there!
     </div>
   )
 }
 
-export default App
